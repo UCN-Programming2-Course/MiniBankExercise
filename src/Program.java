bc55d0f43acc3e23d9946575c8442e035a56c6f5
Lars Nysom <LANY@ucn.dk>
2021-09-15 21:12:43 +0200

    Added comments to main

--- src/Program.java
+++ src/Program.java
@@ -1,9 +1,26 @@
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
 public class Program {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		// TODO: Test your exercise solutions from here, You can use the displayResultSet method.
 
+			
+			
+			
+			
+		
 	}
 
+	private static void displayResultSet(ResultSet rs) throws SQLException {
+		while(rs.next()) {
+			int colCount = rs.getMetaData().getColumnCount();
+			String output = "";
+			for(int i = 1; i <= colCount; i++){
+				output += rs.getString(i) + "\t";
+			}
+			System.out.println(output);
+		}		
+	}
 }
