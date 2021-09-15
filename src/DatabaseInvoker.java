6ac2152f53ba452d5bc068d9e5dd9bd6abbd6fb9
Lars Nysom <LANY@ucn.dk>
2021-09-15 15:10:20 +0200

    Added solution stub

---
+++ src/DatabaseInvoker.java
@@ -0,0 +1,14 @@
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class DatabaseInvoker {
+
+	// TODO: Exercise 1
+	// TODO: Exercise 2
+	// TODO: Exercise 3
+	// TODO: Exercise 4
+	// TODO: Exercise 5
+	// TODO: Exercise 6
+	// TODO: Exercise 7
+}
