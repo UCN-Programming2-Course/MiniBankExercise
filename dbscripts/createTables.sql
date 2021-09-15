489cacc74001f7f4d3ea18bc27d768f663411184
Lars Nysom <LANY@ucn.dk>
2021-09-15 15:07:12 +0200

    Changed DB scripts

--- dbscripts/createTables.sql
+++ dbscripts/createTables.sql
@@ -1,5 +1,13 @@
-DROP database Minibank01;
-CREATE database Minibank01
+USE master;
+
+if exists (select * from sysdatabases where name = 'Minibank01')
+begin
+	DROP database Minibank01;
+end
+
+
+CREATE database Minibank01;
+GO
 
 USE Minibank01;
 
@@ -34,5 +42,29 @@ CREATE TABLE Account(
 		PRIMARY KEY(accNo),
 	CONSTRAINT ClientFK
 		FOREIGN KEY(clientNo) REFERENCES Client(cpr)
-)
+);
+GO
 		
+USE master;
+
+
+CREATE LOGIN minibankuser WITH PASSWORD=N'password', CHECK_EXPIRATION=OFF, CHECK_POLICY=OFF
+GO
+
+USE Minibank01;
+GO
+
+CREATE USER minibankuser FOR LOGIN minibankuser WITH DEFAULT_SCHEMA=[dbo]
+GO
+
+ALTER AUTHORIZATION ON SCHEMA::[db_datareader] TO [minibankuser]
+GO
+
+ALTER AUTHORIZATION ON SCHEMA::[db_datawriter] TO [minibankuser]
+GO
+
+ALTER ROLE [db_datareader] ADD MEMBER [minibankuser]
+GO
+
+ALTER ROLE [db_datawriter] ADD MEMBER [minibankuser]
+GO
