183b7fd3b0229f8e1138543bf8c0e1df7622f7b6
Lars Nysom <LANY@ucn.dk>
2021-09-16 13:40:02 +0200

    Proposed solution

--- src/DatabaseInvoker.java
+++ src/DatabaseInvoker.java
@@ -5,10 +5,120 @@ import java.sql.SQLException;
 public class DatabaseInvoker {
 
 	// TODO: Exercise 1
+	public static ResultSet ex1_SearchInfoForClient(String clientName) throws SQLException {
+
+		final String query = "SELECT Client.cpr, Client.name, Client.status, Account.accNo, Account.balance "
+				+ "FROM Client "
+				+ "JOIN Account ON Account.clientNo = Client.Cpr "
+				+ "WHERE Client.name = ?";
+		PreparedStatement statement = Database.getConnection().prepareStatement(query);
+       
+		statement.setString(1, clientName); 
+		ResultSet rs = statement.executeQuery();
+		return rs;
+	}
+	
 	// TODO: Exercise 2
+	public static ResultSet ex2_GetClientListByStatusAndCity(String status, String city) throws SQLException {
+
+		final String query = "SELECT Client.cpr, Client.name, Client.street, Client.postalCode, PostalCity.city, Client.status "
+				+ "FROM Client "
+				+ "JOIN PostalCity ON PostalCity.postalCode = Client.postalCode "
+				+ "WHERE PostalCity.city = ? "
+				+ "AND Client.status = ?";
+		PreparedStatement statement = Database.getConnection().prepareStatement(query);
+		
+		// set the parameters (bind variables):
+		statement.setString(1, city); 
+		statement.setString(2, status); 
+		ResultSet rs = statement.executeQuery();
+
+		return rs;
+	}	
+		
 	// TODO: Exercise 3
+	public static int ex3_InsertCity(String postalCode, String city) throws SQLException{
+
+		final String query = "INSERT INTO PostalCity values(?, ?);";
+		
+		PreparedStatement statement = Database.getConnection().prepareStatement(query);
+		
+		// set the parameters (bind variables) - the order is important!:
+		statement.setString(1, postalCode);
+		statement.setString(2, city);
+		
+		return statement.executeUpdate();
+	}
+	
 	// TODO: Exercise 4
+	public static ResultSet ex4_GetClientsByArea(String postalCode1, String postalCode2) throws SQLException {
+
+		final String query = "SELECT Client.cpr, Client.name, Client.street, Client.postalCode, PostalCity.city "
+				+ "FROM Client "
+				+ "JOIN PostalCity ON PostalCity.postalCode = Client.postalCode "
+				+ "WHERE PostalCity.postalCode BETWEEN ? AND ?";
+		
+		PreparedStatement statement = Database.getConnection().prepareStatement(query);
+        
+		// set the parameters (bind variables):
+		statement.setString(1, postalCode1); 
+		statement.setString(2, postalCode2); 
+		ResultSet rs = statement.executeQuery();
+
+		return rs;
+	}
+	
 	// TODO: Exercise 5
+	public static int ex5_CreateAccount(String cpr, int accNo, double balance, double intRate) throws SQLException{
+
+		final String query = "INSERT INTO Account values(?, ?, ?, ?);";
+		
+		PreparedStatement statement = Database.getConnection().prepareStatement(query);
+		
+		// set the parameters (bind variables) - the order is important!:
+        statement.setString(4, cpr);
+        statement.setInt(1, accNo);
+        statement.setDouble(2, balance);
+        statement.setDouble(3,intRate);
+		
+        int result = statement.executeUpdate();
+        
+        return result;
+	}
+	
 	// TODO: Exercise 6
+	public static boolean ex6_CheckClientByCpr(String cpr) throws SQLException {
+		
+		final String query = "SELECT * FROM Client WHERE cpr = ?";
+		
+		PreparedStatement statement = Database.getConnection().prepareStatement(query);
+		        
+		// set the parameters (bind variables):
+		statement.setString(1, cpr); 
+		ResultSet rs = statement.executeQuery();
+
+		boolean found = rs.next();
+		return found;
+	}
+
+	
 	// TODO: Exercise 7
+	public static int ex7_CreateAccount(String cpr, String name, String street, String postalCode, int accNo, double balance, double intRate) throws SQLException{
+		
+		if(!ex6_CheckClientByCpr(cpr)) {
+        	// create client
+			String query = "INSERT INTO Client VALUES (?, ?, ?, ?, ?)";
+			PreparedStatement statement = Database.getConnection().prepareStatement(query);
+			statement.setString(1, cpr);
+			statement.setString(2, name);
+			statement.setString(3, street);
+			statement.setString(4, postalCode);
+			statement.setString(5, "A");
+			statement.executeUpdate();
+        }
+
+		int result = ex5_CreateAccount(cpr, accNo, balance, intRate);
+		return result;
+	}
+	
 }
--- src/Program.java
+++ src/Program.java
@@ -4,23 +4,56 @@ import java.sql.SQLException;
 public class Program {
 
 	public static void main(String[] args) {
-		// TODO: Test your exercise solutions from here, You can use the displayResultSet method.
+		// TODO: Test your exercise solutions from here, You can use the
+		// displayResultSet method.
+
+		try {
+
+			// test exercise 1
+			// ResultSet rs = DatabaseInvoker.ex1_SearchInfoForClient("Lars");
+			// displayResultSet(rs);
+
+			// test exercise 2
+			// ResultSet rs = DatabaseInvoker.ex2_GetClientListByStatusAndCity("A",
+			// "Aalborg");
+			// displayResultSet(rs);
+
+			// test exercise 3
+			// int result = DatabaseInvoker.ex3_InsertCity("9500", "Hobro");
+			// System.out.println(result);
+
+			// test exercise 4
+			// ResultSet rs = DatabaseInvoker.ex4_GetClientsByArea("9000", "9700");
+			// displayResultSet(rs);
+
+			// test exercise 5
+			// int result = DatabaseInvoker.ex5_CreateAccount("1234567890", 8, 8765.8, 4.6);
+			// System.out.println(result);
+
+			// test exercise 6
+			// boolean found = DatabaseInvoker.ex6_CheckClientByCpr("1234567890");
+			// System.out.println(found);
+
+			// test exercise 7
+			int result = DatabaseInvoker.ex7_CreateAccount("1234569821", "Kurt", "Z Street", "9500", 10, 1100.00, 2.3);
+			System.out.println(result);
 
 			
-			
-			
-			
-		
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
 	}
 
 	private static void displayResultSet(ResultSet rs) throws SQLException {
-		while(rs.next()) {
+		while (rs.next()) {
 			int colCount = rs.getMetaData().getColumnCount();
 			String output = "";
-			for(int i = 1; i <= colCount; i++){
+			for (int i = 1; i <= colCount; i++) {
 				output += rs.getString(i) + "\t";
 			}
 			System.out.println(output);
-		}		
+		}
 	}
 }
